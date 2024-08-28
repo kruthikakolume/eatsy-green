37a1b57ef950d8028ee7b98f819938ac603ed30a
kruthikakolume <kolume.k@northeastern.edu>
2024-08-28 00:12:10 -0400

    Changed base URL

--- frontend/src/apis/eatsygreen.js
+++ frontend/src/apis/eatsygreen.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-  // baseURL: 'https://eatsy-green.onrender.com',
-  baseURL: 'http://localhost:3000/',
+  baseURL: 'https://eatsy-green.onrender.com',
 });
